37e27c78011a58c963f5447209f5912a136e82fc
OdysseasLS <odysseaslyd@gmail.com>
2025-09-28 21:59:29 +0200

    Update api_client.gd

--- scripts/api_client.gd
+++ scripts/api_client.gd
@@ -4,17 +4,50 @@ extends Node
 This script handles prompting your conversational AI model.
 It is written with OpenAI API in mind but you should be able to modify it
 to use any LLM through API.
+The functions are only the most basic parts of creating a conversational agent,
+you can add your own functions to add diferent actions in this and other scripts.
 """
 
 var API_KEY = ""  # your API key here
 var base_url = "https://api.openai.com/v1/chat/completions"
 
-func get_answer(context):
-	#TODO: finish getting answer logic
+signal response_received(response_text)
+signal error(error_message)
+
+func send_message(message: String, conversation_history: Array=[]) -> void:
+	
 	var http_request = HTTPRequest.new()
 	add_child(http_request)
 	
-	http_request.connect("request_completed", self, "_on_request_completed")
+	http_request.request_completed.connect(_on_request_completed.bind(http_request))
+	
+	var headers = ["Content-Typer: application/json", "Authorization: Bearer" + API_KEY]
+	
+	# messages array to keep history as context
+	var messages = []
+	
+	# set system prompt
+	var system_prompt = _system_prompt()
+	messages.append({"system:": system_prompt})
+	
+	for msg in conversation_history:
+		messages.append(msg)
+		
+	messages.append({"role": "user", "content": message})
+	
+	var body = JSON.stringify({
+		"model": "gpt-3.5-turbo",
+		"messages": messages,
+		"max_tokens": 150,
+		"temperature": 0.7
+		})
+   
+	var error = http_request.request(base_url, headers, HTTPClient.METHOD_POST, body)
+	if error != OK:
+		emit_signal("error_occurred", "Error creating request: " + str(error))
+		http_request.queue_free()
+	
+
 
 func _system_prompt():
 	"""
@@ -29,13 +62,17 @@ func _system_prompt():
 	return system_prompt
 	
 func _on_request_completed(result, response_code, headers, body):
-	# TODO: fix errors
 	if response_code == 200:
-		var json_instance = JSON
-		var json = json_instance.parse(body.get_string_from_utf8())
-		var response_text = json.result.choices[0].message.content
-		return response_text
+		var json = JSON.new()
+		var parse_result = json.parse(body.get_string_from_utf8())
+		
+		if parse_result == OK:
+			var response_data = json.get_data()
+			var response_text = response_data["choices"][0]["message"]["content"]
+			emit_signal("response_recieved", response_text.strip())
+		else:
+			emit_signal("error", "can't parse")
 	else:
-		#TODO: add error message and logic
-		push_error()
+		emit_signal("error", "request failed")
+		
 	
