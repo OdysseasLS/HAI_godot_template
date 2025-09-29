87d3c319e412396ef9dab7413c8b38fbdeedfb85
OdysseasLS <odysseaslyd@gmail.com>
2025-09-29 20:05:06 +0200

    start working on agent scene

---
+++ scripts/agent.gd
@@ -0,0 +1,29 @@
+extends Node
+
+signal conversation_updated(message_history)
+signal response_received(response_text)
+signal error(error_message)
+
+var conversation_history = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if ApiClient:
+		ApiClient.response_received.connect(_on_response)
+		ApiClient.error.connect(_on_error)
+		
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_response(response_text: String):
+	var message = {"role": "AI", "content": response_text}
+	conversation_history.append(message)
+	
+	emit_signal("response_received", response_text)
+	emit_signal("conversation_updated", conversation_history)
+	
+func _on_error(error_message:String):
+	emit_signal("error", error_message)
+	
