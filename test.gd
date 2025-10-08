f7fbf34450c9e4f72b83027044c6cdf136d7f4ac
OdysseasLS <odysseaslyd@gmail.com>
2025-10-08 22:13:24 +0200

    kind of better(?)

--- test.gd
+++ test.gd
@@ -1,11 +1,30 @@
 extends Control
 
+@onready var input_field = $VBoxContainer/InputField
+@onready var chat_display = $VBoxContainer/ChatDisplay
+@onready var send_button = $VBoxContainer/SendButton
+@onready var agent = $Agent
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	send_button.pressed.connect(_on_send_pressed)
+	agent.response_received.connect(_on_agent_response)
+	agent.error.connect(_on_agent_error)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+func _on_send_pressed():
+	var message = input_field.text
+	agent.reply(message)
+	input_field.text=""
+	chat_display.text += "\nYou: " + message
+	
+	
+func _on_agent_response(response_text):
+	chat_display.text += "\nAI: " + response_text
+
+func _on_agent_error(error_message):
+	chat_display.text += "\nError: " + error_message
+	print("Error ", error_message)
